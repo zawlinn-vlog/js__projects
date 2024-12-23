72699293e74650a2baef1ef8d0e0c8e3d33ad426
User <user@Users-MacBook-Pro.local>
2024-12-23 09:33:24 +0700

    JS Small App: Update

---
+++ 02 Project/assets/js/app.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const progressBar = document.querySelector(".progress-gTr");
+
+function greenToRed(step) {
+  // Assuming a maximum step value of 255
+
+  let red = Math.floor((step * 255) / 255); // Red value increases with step
+
+  let green = 255 - Math.floor((step * 255) / 255); // Green value decreases with step
+
+  let blue = 0; // Blue remains constant
+
+  return `rgb(${red}, ${green}, ${blue})`;
+}
+
+window.addEventListener("scroll", function () {
+  //   console.log(this.document.documentElement);
+  const scrollLen = document.documentElement.scrollTop;
+  const scrollH = document.documentElement.scrollHeight;
+  const clientH = document.documentElement.clientHeight;
+
+  const height = scrollH - clientH;
+
+  const width = (scrollLen / height) * 100;
+
+  const colors = Math.round(width * 2.55);
+
+  console.log(colors);
+
+  progressBar.style.width = width.toFixed(2) + "%";
+  progressBar.style.backgroundColor = greenToRed(colors);
+
+  //   console.log(scrollLen, scrollH, clientH, height, width.toFixed(2));
+});
