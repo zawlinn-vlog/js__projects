cff5560adba94385223d83a22716d296e013e5c0
User <user@Users-MacBook-Pro.local>
2024-12-27 10:00:48 +0700

    JS Small App: Update

--- 05 Project/assets/js/app.js
+++ 05 Project/assets/js/app.js
@@ -1,1 +1,22 @@
 "use strict";
+
+$(document).ready(function () {
+  function checkClass(e, str) {
+    return e.target.classList.contains(str);
+  }
+  $("body").on("click", function (e) {
+    console.log(checkClass(e, "signin-btn"));
+
+    if (checkClass(e, "signin-btn") || checkClass(e, "register-btn")) {
+      $(".modals").addClass("active");
+    }
+
+    if (checkClass(e, "modals-shadow")) {
+      $(".modals").removeClass("active");
+    }
+  });
+
+  $(window).on("keyup", function (e) {
+    if (e.code == "Escape") $(".modals").removeClass("active");
+  });
+});
