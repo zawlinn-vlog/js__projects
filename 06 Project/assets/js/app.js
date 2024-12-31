0393f00fe4f0822a01ce9ac4d5db25e709915c4a
User <user@Users-MacBook-Pro.local>
2024-12-31 11:56:19 +0700

    JS Small App: Update

---
+++ 06 Project/assets/js/app.js
@@ -0,0 +1,48 @@
+"use strict";
+
+$(document).ready(function () {
+  function init() {
+    $(".navbars-items").css({
+      "--height": "100%",
+      "--top": "0",
+      "--b-color": "transparent",
+      "--bg-color": "#eee",
+    });
+  }
+
+  console.log($(".navbars-items").get(0));
+
+  function changeElStyle(index) {
+    $($(".navbars-items").get(index)).css({
+      "--height": "calc(100% + 2rem)",
+      "--top": "-2rem",
+      "--b-color": "#cdcdcd",
+      "--bg-color": "#fdfdfd",
+    });
+  }
+
+  $(".navbars-items").on("click", function () {
+    // console.log(this);
+
+    const id = $(this).data("id");
+    console.log(id);
+
+    displayFun(id);
+
+    // changeElStyle(id);÷
+  });
+
+  function activeElement(index) {
+    $(".tabs-items").hide();
+
+    $(`.tabs-items__${index}`).show();
+  }
+
+  function displayFun(index = 0) {
+    init();
+    activeElement(index);
+    changeElStyle(index);
+  }
+
+  displayFun();
+});
