5ec6cd2901b245b6cfc1e05c5021d33a0919af20
User <user@Users-MacBook-Pro.local>
2025-01-10 10:36:28 +0700

    JS Small App: Update

--- 09 Project/assets/js/app.js
+++ 09 Project/assets/js/app.js
@@ -1,10 +1,33 @@
 "use strict";
 
 $(document).ready(function () {
-  let index = 0;
+  const wrapper = document.querySelector(".data-container");
+
+  //
 
   let arr = [],
+    index,
     taskObj;
+
+  const getData = localStorage.getItem("taskUser");
+
+  function loopEl(arrobj) {
+    wrapper.innerHTML = "";
+
+    arrobj.forEach((val, ind) => {
+      createEl(val, ind);
+    });
+  }
+
+  // console.log(getData);
+
+  (function init() {
+    if (getData) {
+      arr = JSON.parse(getData);
+
+      loopEl(arr);
+    }
+  })();
 
   //
 
@@ -14,35 +37,35 @@ $(document).ready(function () {
 
   //
 
-  function createEl() {
+  function createEl(obj, ind) {
     const html = `
-    <li class=" d-flex justify-content-between align-items-center list-group-item data-item">
-           <span class=" data-txt ">${index < 9 ? "0" + ++index : ++index}. ${$(
-      "#inputTxt"
-    ).val()}</span>
+    <li class=" d-flex justify-content-between align-items-center list-group-item data-item" data-id= ${
+      obj.id
+    }>
+           <span class="text-capitalize data-txt ${
+             obj.completed ? "completed" : ""
+           }">${++ind <= 9 ? "0" + ind : ind}. ${obj.taskName}</span>
            <div class="action">
-               <a href="javascript:void(0)" class="btn btn-outline-success px-3 text-capitalize" id="done">done</a>
+               <a href="javascript:void(0)" class="btn btn-outline-success px-3 text-capitalize" id="edit">edit</a>
                <a href="javascript:void(0)" class="btn btn-outline-danger px-3 text-capitalize" id="del">delete</a>
            </div>
        </li>
 `;
 
-    document
-      .querySelector(".data-container")
-      .insertAdjacentHTML("beforeend", html);
+    wrapper.insertAdjacentHTML("beforeend", html);
 
     //
 
-    //
-    taskObj = {
-      id: index,
-      taskName: $("#inputTxt").val().trim().toLowerCase(),
-      completed: document
-        .querySelector(".data-txt")
-        .classList.contains("completed"),
-    };
+    // //
+    // taskObj = {
+    //   id: index,
+    //   taskName: $("#inputTxt").val().trim().toLowerCase(),
+    //   completed: document
+    //     .querySelector(".data-txt")
+    //     .classList.contains("completed"),
+    // };
 
-    arr.push(taskObj);
+    // arr.push(taskObj);
   }
 
   //
@@ -52,20 +75,96 @@ $(document).ready(function () {
 
     if ($("#inputTxt").val() !== "") {
       if (!localStorage.getItem("taskUser")) {
-        createEl();
+        //
+        taskObj = {
+          id: 1,
+          taskName: $("#inputTxt").val().trim().toLowerCase(),
+          completed: false,
+        };
 
-        console.log(arr);
+        // console.log(taskObj);
+        arr.push(taskObj);
+
+        loopEl(arr);
 
         updateLocal(arr);
+
+        // location.reload();
+
+        // console.log(arr);
       } else {
         arr = JSON.parse(localStorage.getItem("taskUser"));
 
-        createEl();
-        updateLocal(arr);
+        if (
+          !arr
+            .map((val) => val.taskName)
+            .includes($("#inputTxt").val().trim().toLowerCase())
+        ) {
+          index = arr[arr.length - 1]?.id ?? 0;
+
+          taskObj = {
+            id: ++index,
+            taskName: $("#inputTxt").val().trim().toLowerCase(),
+            completed: false,
+          };
+
+          arr.push(taskObj);
+
+          loopEl(arr);
+
+          updateLocal(arr);
+
+          // location.reload();
+        } else {
+          alert("Data Exist");
+        }
       }
     } else {
       //   console.log("empty");
       window.alert("Please, Type Something in the input box!");
+    }
+
+    console.log(wrapper.querySelector(".data-item"));
+  });
+
+  $(document).on("contextmenu", function (e) {
+    e.preventDefault();
+
+    if (e.target.closest(".data-item").classList.contains("data-item")) {
+      const getObj = arr.find(
+        (val) => val.id == $(e.target.closest(".data-item")).data("id")
+      );
+
+      e.target
+        .closest(".data-item")
+        .querySelector(".data-txt")
+        .classList.add("completed");
+
+      getObj.completed = true;
+
+      console.log(arr);
+
+      updateLocal(arr);
+    }
+  });
+
+  $(document).on("click", function (e) {
+    console.log(this.querySelector("#del").id, e.target.id);
+
+    if (e.target.closest(".data-item").id == "del") {
+      console.log($(e.target.closest(".data-item")).data("id"));
+
+      const index = arr.findIndex(
+        (val) => val.id == $(e.target.closest(".data-item")).data("id")
+      );
+
+      arr.splice(index, 1);
+
+      console.log(arr);
+
+      updateLocal(arr);
+
+      location.reload();
     }
   });
 });
